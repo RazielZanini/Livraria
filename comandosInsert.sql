733f032ec33012d1a9b25c3836b61ab33a03359d
Raziel Zanini <110948309+RazielZanini@users.noreply.github.com>
2023-07-20 14:08:13 -0300

    Delete comandosInsert.sql

--- comandosInsert.sql
+++
@@ -1,6 +0,0 @@
-insert into cliente (idCliente) value('5');
-insert into cliente (idCliente , nome , tel, endereço , CEP) values ('7' , 'Raziel' , '319999999' , 'casa do caralho' , '32415265');
-
-insert into Compra (idCompra) value('15');
-select * from cliente;
-select * from compra;
